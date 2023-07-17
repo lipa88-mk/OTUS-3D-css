08d1cf6c96c61188ac5ec9f405407cceb1f461a7
Maria Korshunova <65017545+lipa88-mk@users.noreply.github.com>
2023-07-17 17:45:34 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+"use strict"
+
+const item = document.querySelector('.parallax__wrap');
+
+const SPEED = 0.015;
+
+function onCardMove (evt) {
+	const a =  - (evt.offsetY - item.offsetHeight / 2) * SPEED;
+	const b = (evt.offsetX - item.offsetWidth / 2) * SPEED;
+
+	item.setAttribute('style', `transform: rotateX(${a}deg) rotateY(${b}deg)`);
+}
+
+function onCardOut () {
+	item.removeAttribute('style');
+}
+
+item.addEventListener('mousemove', onCardMove);
+item.addEventListener('mouseout', onCardOut);
+
